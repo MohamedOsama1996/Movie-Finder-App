66fb4a4e13923bc9f78a839a57927a21d368333d
MohamedOsama1996 <mohdosama962@gmail.com>
2024-07-13 16:57:19 +0300

    creating project structure

---
+++ src/main/java/com/movie/finder/MovieFinderAppApplication.java
@@ -0,0 +1,13 @@
+package com.movie.finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieFinderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFinderAppApplication.class, args);
+	}
+
+}
